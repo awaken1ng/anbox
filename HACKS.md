028e2c359fb80995424d4b0767ee68aa70abf136
awakening <lucidsunlight@yandex.ru>
2019-01-26 13:53:35 +0700

    Hack to allow non-freeform windows

--- HACKS.md
+++ HACKS.md
@@ -2,3 +2,10 @@
 
 As name suggests, drops Alt-Tab events.
 Can be useful in single-window since alt-tabbing out of it also switches applications in there.
+
+
+- ANBOX_ALLOW_NON_FREEFORM=true|false|"package.name"
+
+Allow windows that are not a part of freeform stack.
+For example, if after running a game there is no window, but you can hear the music in the background, Anbox might have skipped window creation for it because it doens't support freeform.
+
--- src/anbox/wm/multi_window_manager.cpp
+++ src/anbox/wm/multi_window_manager.cpp
@@ -45,7 +45,11 @@ void MultiWindowManager::apply_window_state_update(const WindowState::List &upda
 
   for (const auto &window : updated) {
     // Ignore all windows which are not part of the freeform task stack
-    if (window.stack() != Stack::Id::Freeform) continue;
+    if (window.stack() != Stack::Id::Freeform) {
+      std::string allow_non_freeform = utils::get_env_value("ANBOX_ALLOW_NON_FREEFORM", "false");
+      if (allow_non_freeform == "false") continue;
+      else if (allow_non_freeform != "true" && window.package_name() != allow_non_freeform) continue;
+    }
 
     // And also those which don't have a surface mapped at the moment
     if (!window.has_surface()) continue;
